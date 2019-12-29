d772488461dd348c0a442fd6d888e06d4e446dca
Stackingrule <messageswitching@gmail.com>
2019-12-29 20:33:33 +0800

    update Segmenter

--- 自然语言处理-原理与实现/nlp/src/main/java/ChineseSegmentation/Segmenter.java
+++ 自然语言处理-原理与实现/nlp/src/main/java/ChineseSegmentation/Segmenter.java
@@ -1,11 +1,15 @@
 package ChineseSegmentation;
 
 
+import jdk.nashorn.api.scripting.ScriptObjectMirror;
+
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
+
 
 public class Segmenter {
     String text;    //切分的文本
@@ -18,27 +22,34 @@ public class Segmenter {
 
     private static TSTNode root;
 
-    static {    //加载词典
-        String fileName = "WordList.txt";
-        FileReader fileRead = new FileReader(fileName);
-        BufferedReader read = new BufferedReader(fileRead);
-        String line;    //读入的一行
+    static {//加载词典
+        String fileName = "SDIC.txt";//"WordList.txt";
         try {
-            FileReader fileReader = new FileReader(fileName);
-            while ((line = read.readLine()) != null) {
-                StringTokenizer st = new StringTokenizer(line, "\t");
-                String key = st.nextToken();
-                TSTNode endNode = createNode(key);
+            //FileReader filereadnew = new FileReader(fileName);
+            //BufferedReader read = new BufferedReader(filereadnew);
+            FileInputStream file = new FileInputStream(fileName);
+            BufferedReader in = new BufferedReader(new InputStreamReader(file, "GBK"));
+            String line;
+            try {
+                while ((line = in.readLine()) != null) {
+                    StringTokenizer st = new StringTokenizer(line, "\t");
+                    String key = st.nextToken();
+
+                    TSTNode currentNode = createNode(key);
+                    currentNode.nodeValue = key;
+                    // System.out.println(currentNode);
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }finally {
+                in.close();
             }
-        } catch (IOException e) {
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
-        } finally {
-            return.close;
+        }catch (IOException e) {
+            e.printStackTrace();
         }
-    } catch (FileNotFoundException e) {
-        e.printStackTrace();
-    } catch (IOException e) {
-        e.printStackTrace();
     }
 
 
@@ -67,6 +78,43 @@ public class Segmenter {
 
             if (charComp == 0) {
                 charIndex++;
+            }
+        }
+    }
+
+    public static TSTNode createNode(String key) {
+        int chatIndex = key.length() - 1;
+        char currentChar = key.charAt(chatIndex);
+        if (root == null) {
+            root = new TSTNode(currentChar);
+        }
+
+        TSTNode currentNode = root;
+        while (true) {
+            //比较词的当前字符与节点的当前字符
+            int compa = currentChar - currentNode.splitChar;
+            if (compa == 0) {
+                chatIndex--;    //更新位置
+                if (chatIndex < 0) {    //判断是否已经到头了
+                    return currentNode; //创建完毕，退出循环
+                }
+                currentChar = key.charAt(chatIndex);    //更新当前字符
+                if (currentNode.miNode == null) {
+                    currentNode.miNode= new TSTNode(currentChar);
+                }
+                currentNode = currentNode.miNode;   //向下找
+            }
+            else if (compa < 0) {
+                if (currentNode.loNode == null) {
+                    currentNode.loNode = new TSTNode(currentChar);  //创建左节点
+                }
+                currentNode = currentNode.loNode;
+            }
+            else {
+                if (currentNode.hiNode == null) {
+                    currentNode.hiNode = new TSTNode(currentChar);
+                }
+                currentNode = currentNode.hiNode;
             }
         }
     }
--- 自然语言处理-原理与实现/nlp/src/main/java/ChineseSegmentation/TSTNode.java
+++ 自然语言处理-原理与实现/nlp/src/main/java/ChineseSegmentation/TSTNode.java
@@ -5,11 +5,13 @@ import trigramSeg.EntryWord;
 public class TSTNode {
     public EntryWord data = null;
 
-    private TSTNode loNode;
-    private TSTNode miNode;
-    private TSTNode hiNode;
+    protected TSTNode loNode;
+    protected TSTNode miNode;
+    protected TSTNode hiNode;
 
     protected char splitChar;   //本节点表示的字符
+
+    protected String nodeValue; // 是否可以结束
 
     public TSTNode(char key) {
         this.splitChar = key;
